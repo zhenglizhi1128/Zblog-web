e6b4a42f633a6397cf2e98b420c26553f28e09b8
zhenglizhi1128 <17519148828@163.com>
2021-05-27 19:34:34 +0800

    格式化

--- src/views/System.vue
+++ src/views/System.vue
@@ -1,49 +1,56 @@
 <template>
-  <div>
-    <el-container>
-      <el-aside width="200px">
-        <el-menu
-          class="el-menu-vertical-demo"
-          @open="handleOpen"
-          @close="handleClose"
-        >
-          <el-submenu index="1">
-            <template slot="title">
-              <i class="el-icon-s-operation"></i>
-              <span>系统管理</span>
-            </template>
-            <el-menu-item index="1-1">
-              <template slot="title">
-                <i class="el-icon-location"></i>
-                <el-link :underline="false" href="/labelset">标签管理</el-link>
-              </template>
-            </el-menu-item>
-            <el-menu-item index="1-2">
-              <template slot="title">
-                <i class="el-icon-location"></i>
-                <el-link :underline="false" href="/blogs">无下划线</el-link>
-              </template>
-            </el-menu-item>
-          </el-submenu>
-        </el-menu>
-      </el-aside>
-      <el-main><router-view /></el-main>
-    </el-container>
-  </div>
+	<div>
+		<el-container>
+			<el-aside width="200px">
+				<el-menu
+					class="el-menu-vertical-demo"
+					@open="handleOpen"
+					@close="handleClose"
+				>
+					<el-submenu index="1">
+						<template slot="title">
+							<i class="el-icon-s-operation"></i>
+							<span>系统管理</span>
+						</template>
+						<el-menu-item index="1-1">
+							<template slot="title">
+								<i class="el-icon-location"> </i>
+
+								<el-link :underline="false" href="/labelset"
+									>标签管理
+								</el-link>
+							</template>
+						</el-menu-item>
+						<el-menu-item index="1-2">
+							<template slot="title">
+								<i class="el-icon-location"></i>
+								<el-link :underline="false" href="/blogs"
+									>无下划线
+								</el-link>
+							</template>
+						</el-menu-item>
+					</el-submenu>
+				</el-menu>
+			</el-aside>
+			<el-main>
+				<router-view />
+			</el-main>
+		</el-container>
+	</div>
 </template>
 <script>
 export default {
-  name: "System.vue",
-  data() {
-    return {};
-  },
-  methods: {
-    handleOpen(key, keyPath) {
-      console.log(key, keyPath);
-    },
-    handleClose(key, keyPath) {
-      console.log(key, keyPath);
-    },
-  },
-};
+	name: "System.vue",
+	data() {
+		return {}
+	},
+	methods: {
+		handleOpen(key, keyPath) {
+			console.log(key, keyPath)
+		},
+		handleClose(key, keyPath) {
+			console.log(key, keyPath)
+		}
+	}
+}
 </script>
